3a3f1e8926817c029c6755be2f8bb3112a89e66b
Ephraim Kigamba <ekigamba@ona.io>
2020-06-25 17:07:27 +0300

    Add sample app module

---
+++ app/src/main/java/com/smartregister/client/utils/sample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.smartregister.client.utils.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.smartregister.client.utils.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
