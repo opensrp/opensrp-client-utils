2f931eed1d7086b165053a139529467f979e3680
Ephraim Kigamba <ekigamba@ona.io>
2020-06-25 17:14:44 +0300

    Add opensrp-utils library module

--- app/src/main/java/org/smartregister/client/utils/sample/MainActivity.java
+++ app/src/main/java/org/smartregister/client/utils/sample/MainActivity.java
@@ -1,9 +1,8 @@
-package com.smartregister.client.utils.sample;
+package org.smartregister.client.utils.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.smartregister.client.utils.R;
 
 public class MainActivity extends AppCompatActivity {
 
