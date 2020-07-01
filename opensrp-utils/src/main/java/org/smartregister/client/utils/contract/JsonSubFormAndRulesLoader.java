bf73395961fae1d47be0becd39da47324938a4fb
Ephraim Kigamba <ekigamba@ona.io>
2020-07-01 18:05:28 +0300

    Add setId method on ClientFormContract.Model
    - Delete JsonSubFormAndRulesLoader class

--- opensrp-utils/src/main/java/org/smartregister/client/utils/contract/JsonSubFormAndRulesLoader.java
+++
@@ -1,9 +0,0 @@
-package org.smartregister.client.utils.contract;
-
-/**
- * Created by Ephraim Kigamba - nek.eam@gmail.com on 29-04-2020.
- */
-public interface JsonSubFormAndRulesLoader extends ClientFormContract.Model {
-
-
-}
